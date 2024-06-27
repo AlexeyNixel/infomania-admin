0d21e5ee173b01c6d4241b949f2088d51a11bc62
Pechenkin.A <pechenkin685@gmail.com>
2024-06-27 13:08:32 +0700

    Добавлена админка для подборок книг

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,7 @@ import App from './App.vue';
 import router from './router';
 
 import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-import 'element-plus/theme-chalk/dark/css-vars.css';
-import { vMask } from '@bardoui/vmask';
-
 import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css';
 
 // @ts-ignore
 import CKEditor from '@ckeditor/ckeditor5-vue';
@@ -23,7 +18,6 @@ const app = createApp(App);
 
 app.component('VueDatePicker', VueDatePicker);
 
-app.directive('mask', vMask);
 app.use(CKEditor);
 app.use(createPinia());
 app.use(router);
